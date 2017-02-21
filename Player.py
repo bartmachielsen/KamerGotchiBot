6113179a6759fd5b1aad4d6665348b345961971e
Bart Machielsen <bartmachielsen@gmail.com>
2017-02-21 08:52:46 +0100

    Added gotchi credentials file

--- Player.py
+++ Player.py
@@ -4,30 +4,56 @@ import time
 import random
 from datetime import datetime
 
-playerdetails = {'x-player-token': 'android:d3638728cc6d68b6', 'Content-Type': 'application/json', 'User-agent': 'okhttp/3.4.1'}
-
+player_details = {'x-player-token': '', 'Content-Type': 'application/json', 'User-agent': 'okhttp/3.4.1'}
+gotchi_file = "gotchi_details.json"
 bar_objects = ['food', 'attention', 'knowledge']
 
+def load_gotchi_details():
+    try:
+        return json.loads(open(gotchi_file).read())['user-token']
+    except:
+        return None
 
 def get_score(json_data):
-    return json.loads(json_data)['game']['score']
+    print(json_data)
+    json_conv = json.loads(json_data)
+    if 'game' in json_conv:
+        if 'score' in json_conv['game']:
+            return json.loads(json_data)['game']['score']
+    return "error"
+
 
 def get_care_left(json_data):
     return json.loads(json_data)['game']['careLeft']
 
+
 def get_care_reset(json_data):
     date = datetime.now()
-    splitted = json.loads(json_data)['game']['careReset'].split('T')[1].replace("Z", "").split('.')[0].split(':')
-    return date.replace(hour=int(splitted[0])+1, minute=int(splitted[1]), second=int(splitted[2]))
+    try:
+        time_data = json.loads(json_data)['game']['careReset'].split('T')[1].replace("Z", "").split('.')[0].split(':')
+        return date.replace(hour=int(time_data[0])+1, minute=int(time_data[1]), second=int(time_data[2]))
+    except:
+        return date
 
 
-while True:
-    request = Webparser.get('https://api.kamergotchi.nl/game', playerdetails)
-    cure_reset = get_care_reset(request)
-    if cure_reset > datetime.now() and get_care_left(request) == 0:
-        wait_time = (cure_reset-datetime.now()).seconds
-        print("SLEEPING FOR " + str(wait_time))
-        time.sleep(wait_time)
+if __name__ == '__main__':
+    player_key = load_gotchi_details()
+    if player_key is None:
+        print("player details could net be loaded!")
+    else:
+        player_details['x-player-token'] = player_key
+        print("loaded user details: " + player_key)
 
-    print(get_score(Webparser.post('https://api.kamergotchi.nl/game/care',
-                                   json.dumps({'bar': bar_objects[random.randint(0, len(bar_objects)-1)]}), playerdetails)))
+        while True:
+            request = Webparser.get('https://api.kamergotchi.nl/game', player_details)
+            cure_reset = get_care_reset(request)
+            if cure_reset > datetime.now() and get_care_left(request) == 0:
+                wait_time = (cure_reset-datetime.now()).seconds
+                if wait_time > 600:
+                    wait_time = 600
+                print("waiting " + str(wait_time) + " seconds before continuing")
+                time.sleep(wait_time)
+
+            bar_change = bar_objects[random.randint(0, len(bar_objects)-1)]
+            post_data = Webparser.post('https://api.kamergotchi.nl/game/care', json.dumps({'bar': bar_change}), playerdetails)
+            print("changed " + bar_change + " to score --> " + get_score(post_data))
